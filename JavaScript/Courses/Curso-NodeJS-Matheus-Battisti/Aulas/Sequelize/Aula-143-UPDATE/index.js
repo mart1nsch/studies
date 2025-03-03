03465c2e7b18a1c61a83cf461e6b0d906feb105c
Martin Schneider <martin.schneider250701@gmail.com>
2025-03-03 20:17:35 -0300

    finish sequelize

---
+++ JavaScript/Courses/Curso-NodeJS-Matheus-Battisti/Aulas/Sequelize/Aula-143-UPDATE/index.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const exphbs = require('express-handlebars');
+const conn = require('./db/conn');
+const User = require('./models/User');
+
+const app = express();
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
+app.get('/', async (req, res) => {
+
+    const users = await User.findAll({ raw: true });
+
+    res.render('home', { users: users });
+
+});
+
+app.get('/users/create', (req, res) => {
+
+    res.render('adduser');
+
+});
+
+app.get('/users/:id', async (req, res) => {
+
+    const id = req.params.id;
+    
+    const user = await User.findOne({ where: { id: id } , raw: true});
+
+    res.render('userview', { user: user });
+
+});
+
+app.get('/users/edit/:id', async (req, res) => {
+
+    const id = req.params.id;
+    
+    const user = await User.findOne({ where: { id: id } , raw: true});
+
+    res.render('useredit', { user: user });
+
+});
+
+app.post('/users/create', async (req, res) => {
+
+    const name = req.body.name;
+    const occupation = req.body.occupation;
+    let newsletter = req.body.newsletter;
+
+    if (newsletter === 'on') {
+        newsletter = true;
+    }
+
+    await User.create({ name, occupation, newsletter });
+
+    res.redirect('/');
+
+});
+
+app.post('/users/delete', async (req, res) => {
+
+    const id = req.body.id;
+
+    await User.destroy({ where: { id: id } });
+
+    res.redirect('/');
+
+});
+
+app.post('/users/edit', async (req, res) => {
+
+    const id = req.body.id;
+    const name = req.body.name;
+    const occupation = req.body.occupation;
+
+    const data = {
+        id: id,
+        name: name,
+        occupation: occupation
+    };
+
+    await User.update(data, { where: { id: id } });
+
+    res.redirect('/');
+
+});
+
+conn.sync()
+.then(() => {
+    app.listen(3000);
+})
+.catch(err => console.log(err));
