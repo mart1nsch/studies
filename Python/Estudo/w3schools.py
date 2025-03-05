14cee9276b839a78f8e5792ed092ee8c122ea925
Martin Schneider <martin.schneider250701@gmail.com>
2025-03-05 20:50:01 -0300

    started studies with python

---
+++ Python/Estudo/w3schools.py
@@ -0,0 +1,15 @@
+# Mostra dado em tela
+print('Hello World!')
+
+# Varíaveis
+a = 3
+b = 2
+print(a + b)
+
+# IF, ELIF, ELSE
+if b > a:
+    print("b é maior que a")
+elif b == a:
+    print("b é igual a a")
+else:
+    print("b é menor que a")
