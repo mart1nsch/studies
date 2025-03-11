e5160bd0ee60322b086dfcdd6f11007f7b176763
Martin Schneider <martin.schneider250701@gmail.com>
2025-03-10 21:14:19 -0300

    mais coisas

--- JavaScript/Courses/Curso-NodeJS-Matheus-Battisti/Projetos/Toughts/index.js
+++ JavaScript/Courses/Curso-NodeJS-Matheus-Battisti/Projetos/Toughts/index.js
@@ -3,9 +3,53 @@ const exphbs = require('express-handlebars');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
 const flash = require('express-flash');
-const conn = require('./db/conn');
 
 const app = express();
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// Connection
+const conn = require('./db/conn');
+
+// Models
+const Tought = require('./models/Tought');
+const User = require('./models/User');
+
+// Sessions
+app.use(session({
+    name: "session",
+    secret: "nosso_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+        logFn: function () {},
+        path: require('path').join(require('os').tmpdir(), 'sessions')
+    }),
+    cookie: {
+        secure: false,
+        maxAge: 360000,
+        expires: new Date(Date.now() + 360000),
+        httpOnly: true
+    }
+}))
+
+// Flash Messages
+app.use(flash());
+
+app.use((req, res, next) => {
+
+    if (req.session.userid) {
+        res.locals.session = req.session;
+    }
+
+    next();
+
+});
 
 conn.sync()
 .then(() => {
