c28ef0c1f57a1b65a83a43a2803473ef950e6474
Martin Schneider <martin.schneider250701@gmail.com>
2025-03-18 20:44:42 -0300

    finish avl tree

--- Unisinos/Algoritmos e Programação Árvores e Ordenação/desafio/AVLTree.java
+++ Unisinos/Algoritmos e Programação Árvores e Ordenação/desafio/AVLTree.java
@@ -2,6 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class AVLTree {
+
+    public class Node {
+        int value;
+        Node left, right;
+        int height;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
     private Node root;
 
     public void insert(int value) {
@@ -10,35 +21,72 @@ public class AVLTree {
 
     public Node insert(Node node, int value) {
         if (node == null) {
-            return new Node(value);
-        }
-
-        if (value < node.value) {
+            node = new Node(value);
+        } else if (value < node.value) {
             node.left = insert(node.left, value);
         } else if (value > node.value) {
             node.right = insert(node.right, value);
+        }
+
+        if (this.balancingFactor(node) == 2) {
+            if (this.balancingFactor(node.left) > 0) {
+                node = this.rightRotate(node);
+            } else {
+                node = this.doubleRightRotate(node);
+            }
+        } else if (this.balancingFactor(node) == -2) {
+            if (this.balancingFactor(node.right) < 0) {
+                node = this.leftRotate(node);
+            } else {
+                node = this.doubleLeftRotate(node);
+            }
+        }
+
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+
+        return node;
+    }
+
+    public void remove(int value) {
+        if (search(this.root, value)) {
+            this.root = this.remove(this.root, value);
+        }
+    }
+
+    private Node remove(Node node, int value) {
+        if (node == null) return null;
+
+        if (value < node.value) {
+            node.left = remove(node.left, value);
+        } else if (value > node.value) {
+            node.right = remove(node.right, value);
         } else {
-            return node;
+            if (node.left == null || node.right == null) {
+                node = node.left == null ? node.right : node.left;
+            } else {
+                Node nodeSucessor = findMin(node.right);
+                node.value = nodeSucessor.value;
+                node.right = remove(node.right, nodeSucessor.value);
+            }
         }
 
-        node.height = calculateHeight(node.left, node.right);
+        if (node == null) return null;
 
-        int balancingFactor = this.balancingFactor(node);
+        if (this.balancingFactor(node) == 2) {
+            if (this.balancingFactor(node.left) > 0) {
+                node = this.rightRotate(node);
+            } else {
+                node = this.doubleRightRotate(node);
+            }
+        } else if (this.balancingFactor(node) == -2) {
+            if (this.balancingFactor(node.right) < 0) {
+                node = this.leftRotate(node);
+            } else {
+                node = this.doubleLeftRotate(node);
+            }
+        }
 
-        if (balancingFactor > 1 && value < node.left.value) {
-            return rightRotate(node);
-        }
-        if (balancingFactor < -1 && value > node.right.value) {
-            return leftRotate(node);
-        }
-        if (balancingFactor > 1 && value > node.left.value) {
-            node.left = leftRotate(node.left);
-            return rightRotate(node);
-        }
-        if (balancingFactor < -1 && value < node.right.value) {
-            node.right = rightRotate(node.right);
-            return leftRotate(node);
-        }
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
 
         return node;
     }
@@ -51,8 +99,8 @@ public class AVLTree {
 
         nodeLeft.right = node;
 
-        node.height = calculateHeight(node.left, node.right);
-        nodeLeft.height = calculateHeight(nodeLeft.left, nodeLeft.right);
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+        nodeLeft.height = Math.max(height(nodeLeft.left), node.height) + 1;
 
         return nodeLeft;
     }
@@ -61,26 +109,28 @@ public class AVLTree {
         if (node.right == null) return node;
 
         Node nodeRight = node.right;
-        Node nodeLeft = nodeRight.left;
+        node.right = nodeRight.left;
 
         nodeRight.left = node;
-        node.right = nodeLeft;
 
-        node.height = calculateHeight(node.left, node.right);
-        nodeRight.height = calculateHeight(nodeRight.left, nodeRight.right);
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+        nodeRight.height = Math.max(height(nodeRight.right), node.height) + 1;
 
         return nodeRight;
     }
 
-    private int calculateHeight(Node nodeLeft, Node nodeRight) {
-        int left = nodeLeft == null ? -1 : nodeLeft.height;//height(nodeLeft);
-        int right = nodeRight == null ? -1 : nodeRight.height;//height(nodeRight);
+    private Node doubleRightRotate(Node node) {
+        node.left = this.leftRotate(node.left);
+        return rightRotate(node);
+    }
 
-        return Math.max(left, right) + 1;
+    private Node doubleLeftRotate(Node node) {
+        node.right = this.rightRotate(node.right);
+        return leftRotate(node);
     }
 
     private int height(Node node) {
-        return node == null ? 0 : node.height;
+        return node == null ? -1 : node.height;
     }
 
     private int balancingFactor(Node node) {
@@ -118,7 +168,6 @@ public class AVLTree {
     public List<Integer> postOrder() {
         List<Integer> result = new ArrayList<>();
         postOrder(this.root, result);
-        System.out.println(result);
         return result;
     }
 
@@ -128,6 +177,24 @@ public class AVLTree {
             postOrder(node.right, result);
             result.add(node.value);
         }
+    }
+
+    public boolean search(int value) {
+        return search(root, value);
+    }
+
+    private boolean search(Node node, int value) {
+        while (node != null) {
+            if (node.value == value) {
+                return true;
+            } else if (value < node.value) {
+                node = node.left;
+            } else {
+                node = node.right;
+            }
+        }
+
+        return false;
     }
 
     public void showTree() {
@@ -140,5 +207,15 @@ public class AVLTree {
             showTree(node.left, prefix + (right ? "    " : "│   "), false);
             showTree(node.right, prefix + (right ? "    " : "│   "), true);
         }
+    }
+
+    private Node findMin(Node node) {
+        if (node == null) return null;
+
+        while (node.left != null) {
+            node = node.left;
+        }
+
+        return node;
     }
 }
