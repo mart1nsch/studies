b94997e5dba72a3edf33abcf053f5f6c55fa321a
Martin Schneider <martin.schneider250701@gmail.com>
2025-05-31 13:20:07 -0300

    more js

---
+++ JavaScript/Courses/Curso-Codifica/Aprendizados/recursão.js
@@ -0,0 +1,28 @@
+const prompt = require('prompt-sync')();
+
+// Faz uma soma recursiva de 1 até o número digitado
+/*const numero = Number(prompt("Digite um número para somar de 1 até o número: "));
+
+function soma(n) {
+    if (n === 1) {
+        return n;
+    }
+    return n + soma(n-1);
+}
+
+console.log(soma(numero));*/
+
+// Faz o fibonacci recursivo
+/*const numeroVezesFibonacci = Number(prompt("Quantos números da sequência fibonacci quer ver: "));
+
+
+
+function fibonacci(n) {
+    if (n === 0 || n === 1) {
+        return n;
+    }
+
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+
+console.log(fibonacci(numeroVezesFibonacci));*/
